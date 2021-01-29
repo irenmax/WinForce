6a0e092d03daa3cdc644a7ba225ad39ce479a49f
irenmax <e1634015@student.tuwien.ac.at>
2021-01-29 12:24:35 +0100

    added grid to image for debugging

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -100,6 +100,24 @@ while cap.isOpened():
   # Draw the hand annotations on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  height = image.shape[0]
+  width = image.shape[1]
+  color = (255, 0, 0)
+  thickness = 2
+
+  left_x = int(width * 0.4)
+  right_x = int(width *0.6)
+  upper_y = int(height * 0.4)
+  lower_y = int(height * 0.6)
+
+  # draw grid on image
+  image = cv2.line(image, (left_x, 0), (left_x, height), color, thickness)
+  image = cv2.line(image, (right_x, 0), (right_x, height), color, thickness)
+  image = cv2.line(image, (0, upper_y), (width, upper_y), color, thickness)
+  image = cv2.line(image, (0, lower_y), (width, lower_y), color, thickness)
+
+
+
   if results.multi_hand_landmarks:
     for hand_landmarks in results.multi_hand_landmarks:
       mp_drawing.draw_landmarks(
